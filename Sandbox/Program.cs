240cd508d4bf7d1f9b20807792699b1746cccc42
Ephraim Floyd Magopa <ephraim.magopa@gmail.com>
2020-02-11 08:19:30 +0200

    Add project files.

---
+++ Sandbox/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yesCounter = 1;
+            int noCounter = 2;
+            int totalVotes = yesCounter + noCounter;
+            var yesPercentage = Math.Round((yesCounter * 100.00) / totalVotes,2);
+            var noPercentage = Math.Round((noCounter * 100.00) / totalVotes,2);
+
+            Console.WriteLine($"Total Yes Precentage {yesPercentage}%");
+            Console.WriteLine($"Total No Precentage {noPercentage}%");
+
+            if (yesCounter > noCounter)
+            { 
+                Console.WriteLine($"Yes Votes Win: {yesCounter}"); 
+            }
+            else if(noCounter > yesCounter)
+            {
+                Console.WriteLine($"No Votes Win: {noCounter}");
+            }
+            else
+            {
+                Console.WriteLine("No Winner! It is a Draw");
+            }
+        }
+    }
+}
